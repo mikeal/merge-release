f0002b6c9710f818b9385aafeb1bde994fe3b370
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-02-02 09:07:12 -0800

    fix: absolute import path

--- merge-release-run.js
+++ merge-release-run.js
@@ -2,6 +2,7 @@
 console.log(process.argv)
 
 const fs = require('fs')
+const path = require('path')
 const { execSync } = require('child_process')
 
 /* Configure git */
@@ -20,7 +21,7 @@ if (messages.map(message => message.includes('BREAKING CHANGE')).includes(true))
   version = 'minor'
 }
 
-let pkg = require('./package.json')
+let pkg = require(path.join(process.cwd(), 'package.json'))
 let current = execSync(`npm view ${pkg.name} version`).toString()
 process.stdout.write(execSync(`npm version --allow-same-version=true --git-tag-version=false ${current} `))
 let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString()
