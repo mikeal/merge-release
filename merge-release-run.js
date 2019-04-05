a9ab9607e46ae0cd72a79cf32e203243321deab6
Olle Jonsson <olle.jonsson@gmail.com>
2019-04-05 14:08:13 +0200

    Clean up commented-out code

--- merge-release-run.js
+++ merge-release-run.js
@@ -5,11 +5,6 @@ const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
 
-/* Configure git */
-
-//fs.writeFileSync('git-credentials')
-//execSync('git 
-
 const event = JSON.parse(fs.readFileSync('/github/workflow/event.json').toString())
 
 let messages = event.commits.map(commit => commit.message)
@@ -27,8 +22,3 @@ process.stdout.write(execSync(`npm version --allow-same-version=true --git-tag-v
 let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString()
 console.log(newVersion)
 process.stdout.write(execSync(`npm publish --access=public`))
-/*
-process.stdout.write(execSync(`git tag -a ${newVersion} -m "Created by merge-release.\n\n##Changelog:\n${messages.join('\n')}"`))
-process.stdout.write(execSync(`git push --tags`))
-*/
-// npm version minor && npm publish --access=public && git push --tags
