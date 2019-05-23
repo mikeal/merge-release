53a92ca2d1ea3c55977f44d93e48e31e37d0bc69
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-05-23 18:13:42 +0000

    feat: add git sha to package json

--- merge-release-run.js
+++ merge-release-run.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-console.log(process.argv)
-
 const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process')
@@ -11,12 +9,17 @@ let messages = event.commits.map(commit => commit.message)
 
 let version = 'patch'
 if (messages.map(message => message.includes('BREAKING CHANGE')).includes(true)) {
-  version = 'major' 
+  version = 'major'
 } else if (messages.map(message => message.toLowerCase().startsWith('feat')).includes(true)) {
   version = 'minor'
 }
 
 let pkg = require(path.join(process.cwd(), 'package.json'))
+
+pkg.gitsha = process.env.GITHUB_SHA
+
+fs.writeFileSync(path.join(process.cwd(), 'package.json'), Buffer.from(JSON.stringify(pkg)))
+
 let current = execSync(`npm view ${pkg.name} version`).toString()
 process.stdout.write(execSync(`npm version --allow-same-version=true --git-tag-version=false ${current} `))
 let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString()
