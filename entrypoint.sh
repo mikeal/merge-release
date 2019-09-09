bff8e70f4ac98d2612c6dde848bcf7d88a5b8b6a
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-09-09 23:13:08 +0000

    feat: configure repo for push

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-https://registry.npmjs.org}"
 
   # Allow registry.npmjs.org to be overridden with an environment variable
   printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
@@ -13,6 +13,13 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
 fi
 
 sh -c "git checkout master"
+
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+sh -c 'git config user.name "Automated Publisher"'
+sh -c 'git config user.email "publish-to-github-action@users.noreply.github.com"'
+sh -c 'git remote rm origin'
+sh -c 'git remote add origin "${remote_repo}"'
 
 if [ "$GITHUB_REPOSITORY" = "mikeal/merge-release" ]
 then
