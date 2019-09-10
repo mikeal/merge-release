e8b618992ba47d978a29eb8d7cc01a01c4f076cd
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-09-10 00:21:38 +0000

    fix: pull latest npm config

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,17 @@ set -e
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-https://registry.npmjs.org}"
+  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+  NPM_STRICT_SSL="${NPM_STRICT_SSL-true}"
+  NPM_REGISTRY_SCHEME="https"
+  if ! $NPM_STRICT_SSL
+  then
+    NPM_REGISTRY_SCHEME="http"
+  fi
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
+
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
