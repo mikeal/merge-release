03ebdb014964f6af4486e5dbc4e1241f5a6f1067
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-02-03 12:32:05 -0800

    debug: config sslVerify false

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ fi
 
 # initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git config http.sslVerify false
 git config user.name "Merge Release"
 git config user.email "actions@users.noreply.github.com"
 git remote add merge-release "${remote_repo}"
