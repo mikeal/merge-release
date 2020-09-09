c6b405755f11ad85e803b357e10345962900822d
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-09-09 21:35:14 +0000

    fix: tagging

--- src/merge-release-run.js
+++ src/merge-release-run.js
@@ -76,7 +76,8 @@ const run = async () => {
   setVersion(currentVersion)
   console.log('current:', currentVersion, '/', 'version:', version)
   let newVersion = execSync(`npm version --git-tag-version=false ${version}`, { cwd: srcPackageDir }).toString()
-  setVersion(newVersion.slice(1).replace(/(\r\n|\n|\r)/gm, ""))
+  newVersion = newVersion.replace(/(\r\n|\n|\r)/gm, "")
+  setVersion(newVersion.slice(1))
   console.log('new version:', newVersion)
 
   if (pkg.scripts && pkg.scripts.publish) {
